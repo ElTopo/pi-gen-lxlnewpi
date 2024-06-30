9b83a32a81b9b6ce464f5f82356d1550ff3a3535
El Topo <eltopo@live.com>
2024-06-30 16:18:12 -0700

    added lxl's custom build settings: 00-packages (packages), 02-run.sh (custom fs, remove some packages)

---
+++ stage2/66-lxl-custom/02-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# make sure ssh server is started
+touch ${ROOTFS_DIR}/boot/firmware/ssh
+
+# update fstab with lxl's tmpfs partitions
+cat ${BASE_DIR}/lxlnewpi-dfs/lxl-tmpfs-fstab >> ${ROOTFS_DIR}/etc/fstab
+cp -f ${BASE_DIR}/lxlnewpi-dfs/lxlp  ${ROOTFS_DIR}/home/lxl/
+
+# lxl: remove package(s): dphys-swapfile,fake-hwclock
+#      and do apt autoremove
+on_chroot << EOF
+apt purge -y dphys-swapfile fake-hwclock
+apt autoremove -y
+EOF
+
