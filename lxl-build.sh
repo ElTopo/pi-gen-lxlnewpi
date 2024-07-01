6a0a0b90eaa8050d691dc75b373895fdc87fd1d3
El Topo <eltopo@live.com>
2024-07-01 16:13:50 -0700

    changed $IMG_NAME,$PI_GEN, and $PI_GEN_REPO, with lxlnewpi; also added ips() in lxlp

--- lxl-build.sh
+++ lxl-build.sh
@@ -2,8 +2,18 @@
 
 # NOTE: to clean build, sudo rm -rf work/
 
+APP=$(basename $0)
+
 # build lxlnewpios lite (stage2)
 touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
 touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 
+# source my config file so the script gets env $IMG_NAME
+source ./lxlnewpi-dfs/lxlnewpi-config
+
+echo "$APP: start building [$IMG_NAME]..."
+
 sudo ./build.sh -c ./lxlnewpi-dfs/lxlnewpi-config
+
+[ $? -eq 0 ] && echo "$APP: built OK." || echo "$APP: built failed with problem(s)!"
+echo "$APP: Check [work/$IMG_NAME/build.log] for details."
