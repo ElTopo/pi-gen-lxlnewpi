043ab2ee2021144f80dcc83769a0b8255533a856
El Topo <eltopo@live.com>
2024-06-30 20:51:05 -0700

    added more packages to install: apt-file, apt-show-versions removed more pre-installed: udisks2

--- stage2/66-lxl-custom/02-run.sh
+++ stage2/66-lxl-custom/02-run.sh
@@ -7,10 +7,10 @@ touch ${ROOTFS_DIR}/boot/firmware/ssh
 cat ${BASE_DIR}/lxlnewpi-dfs/lxl-tmpfs-fstab >> ${ROOTFS_DIR}/etc/fstab
 cp -f ${BASE_DIR}/lxlnewpi-dfs/lxlp  ${ROOTFS_DIR}/home/lxl/
 
-# lxl: remove package(s): dphys-swapfile,fake-hwclock
+# lxl: remove package(s): dphys-swapfile,fake-hwclock,udisks2
 #      and do apt autoremove
 on_chroot << EOF
-apt purge -y dphys-swapfile fake-hwclock
+apt purge -y dphys-swapfile fake-hwclock udisks2
 apt autoremove -y
 EOF
 
