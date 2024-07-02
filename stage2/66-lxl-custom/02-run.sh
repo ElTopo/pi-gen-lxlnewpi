15f7a5489715a5869947651044c6f826b7f395ad
El Topo <eltopo@live.com>
2024-07-02 16:07:58 -0700

    fixed file permission for lxlp and lxl-rb2.sh

--- stage2/66-lxl-custom/02-run.sh
+++ stage2/66-lxl-custom/02-run.sh
@@ -13,13 +13,13 @@ mkdir -p ${ROOTFS_DIR}/root/bin/
 cp -f  ${BASE_DIR}/lxlnewpi-dfs/cron-boot.sh ${ROOTFS_DIR}/root/bin/
 # update ~/.profile
 echo " "  >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
-echo "# lxlp, a temp solution before setting up your dotdfs"  >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
-echo ". ~/lxlp"  >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
+echo "# lxlp, a temp solution before setting up your dotdfs" >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
+echo ". ~/lxlp" >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
 # create ~/lxlp
-install -v -o 1000 -g 1000 -D ${BASE_DIR}/lxlnewpi-dfs/lxlp ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/lxlp
+install -v -o 1000 -g 1000 -m 0644 -D ${BASE_DIR}/lxlnewpi-dfs/lxlp ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/lxlp
 # create ~/bin/lxl-rb2.sh
 install -v -o 1000 -g 1000 -d ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bin
-install -v -o 1000 -g 1000 -D  ${BASE_DIR}/lxlnewpi-dfs/lxl-rb2.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bin/lxl-rb2.sh
+install -v -o 1000 -g 1000 -m 0755 -D ${BASE_DIR}/lxlnewpi-dfs/lxl-rb2.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bin/lxl-rb2.sh
 
 # lxl: remove package(s): dphys-swapfile,fake-hwclock,udisks2
 #      and do apt autoremove
