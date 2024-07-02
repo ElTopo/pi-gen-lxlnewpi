c6c6ee60c7bb18e67b0ed9303c47dd514107758b
El Topo <eltopo@live.com>
2024-07-02 14:52:01 -0700

    added /etc/cron.d/lxl-cron-boot and /root/bin/cron-boot.sh for creating dirs in /var/cache; update ~/.profile

---
+++ lxlnewpi-dfs/cron-boot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# this file will be installed in /root/bin, and called by cron(/etc/cron.d/lxl-cron-boot
+
+# this is a temp solution for /var/cache in tmpfs
+
+# for /var/cache in tmpfs, create these directories
+# apt and apt-show-versions might be in tmpfs, create them if necessary
+mkdir -p /var/cache/apt
+mkdir -p /var/cache/apt-show-versions
+
+# some packages also assume folders created in /var/cache
+# such lighttpd/samba/supervisor/polipo/exim4, etc.
+
+# allow normal user to run dmesg
+echo 0 > /proc/sys/kernel/dmesg_restrict
--- stage2/66-lxl-custom/02-run.sh
+++ stage2/66-lxl-custom/02-run.sh
@@ -3,13 +3,22 @@
 # make sure ssh server is started
 touch ${ROOTFS_DIR}/boot/firmware/ssh
 
-# update fstab with lxl's tmpfs partitions
+# update /etc/fstab with lxl's tmpfs partitions
 cat ${BASE_DIR}/lxlnewpi-dfs/lxl-tmpfs-fstab >> ${ROOTFS_DIR}/etc/fstab
+# create /etc/cron.d/lxl-cron-boot
+mkdir -p ${ROOTFS_DIR}/etc/cron.d/
+cp -f  ${BASE_DIR}/lxlnewpi-dfs/lxl-cron-boot ${ROOTFS_DIR}/etc/cron.d/
+# create /root/bin/cron-boot.sh
+mkdir -p ${ROOTFS_DIR}/root/bin/
+cp -f  ${BASE_DIR}/lxlnewpi-dfs/cron-boot.sh ${ROOTFS_DIR}/root/bin/
+# update ~/.profile
 echo " "  >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
 echo "# lxlp, a temp solution before setting up your dotdfs"  >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
 echo ". ~/lxlp"  >> ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.profile
-install -v -o 1000 -g 1000 -d ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bin
+# create ~/lxlp
 install -v -o 1000 -g 1000 -D ${BASE_DIR}/lxlnewpi-dfs/lxlp ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/lxlp
+# create ~/bin/lxl-rb2.sh
+install -v -o 1000 -g 1000 -d ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bin
 install -v -o 1000 -g 1000 -D  ${BASE_DIR}/lxlnewpi-dfs/lxl-rb2.sh ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bin/lxl-rb2.sh
 
 # lxl: remove package(s): dphys-swapfile,fake-hwclock,udisks2
