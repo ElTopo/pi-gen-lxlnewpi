fcdff036e5f7a231f5e707020997bc5ca8e8b188
El Topo <eltopo@live.com>
2024-07-03 16:09:55 -0700

    added vim modeline

--- lxlnewpi-dfs/cron-boot.sh
+++ lxlnewpi-dfs/cron-boot.sh
@@ -2,7 +2,11 @@
 
 # this file will be installed in /root/bin, and called by cron(/etc/cron.d/lxl-cron-boot
 
+PROG=$(basename $0)
+
 # this is a temp solution for /var/cache in tmpfs
+
+logger -t $PROG "starting..."
 
 # for /var/cache in tmpfs, create these directories
 # apt and apt-show-versions might be in tmpfs, create them if necessary
@@ -14,3 +18,5 @@ mkdir -p /var/cache/apt-show-versions
 
 # allow normal user to run dmesg
 echo 0 > /proc/sys/kernel/dmesg_restrict
+
+# vim: set sw=2 et ts=2:
